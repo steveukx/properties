bf2ab8af35f56aaa7327e5e72b0b97d90322f2ca
Steve King <steve@mydev.co>
2013-04-16 14:32:45 +0100

    Documentation
    Add basic section support when reading a file

--- src/PropertiesReader.js
+++ src/PropertiesReader.js
@@ -17,6 +17,12 @@
    }
 
    /**
+    * @type {String} The name of a section that should be prefixed on an property as it is added
+    * @ignore
+    */
+   PropertiesReader.prototype._section = '';
+
+   /**
     * Gets the number of properties that have been read into this PropertiesReader.
     *
     * @name PropertiesReader#length
@@ -39,8 +45,28 @@
     * @return {PropertiesReader} this instance
     */
    PropertiesReader.prototype.read = function(input) {
-      ('' + input).split('\n').forEach(this._addProperty, this);
+      delete this._section;
+      ('' + input).split('\n').forEach(this._readLine, this);
       return this;
+   };
+
+   /**
+    * Used as a processor for the array of input lines when reading from a source file
+    * @param {String} propertyString
+    */
+   PropertiesReader.prototype._readLine = function(propertyString) {
+      if(!!(propertyString = propertyString.trim())) {
+         var section = /^\[([a-zA-Z0-9]+)\]$/.exec(propertyString);
+         var property = !section && /^([^=]+)(={0,1})(.*)$/.exec(propertyString);
+
+         if(section) {
+            this._section = section[1];
+         }
+         else if(property) {
+            section = this._section ? this._section + '.' : '';
+            this.set(section + property[1].trim(), property[3].trim());
+         }
+      }
    };
 
    /**
@@ -57,45 +83,59 @@
       return this;
    };
 
+   /**
+    * Gets a single property value based on the full string key. When the property is not found in the PropertiesReader,
+    * the return value will be null.
+    *
+    * @param {String} key
+    * @return {*}
+    */
    PropertiesReader.prototype.get = function(key) {
       return this._properties.hasOwnProperty(key) ? this._properties[key] : null;
    };
 
+   /**
+    * Sets the supplied key in the properties store with the supplied value, the value can be any string representation
+    * that would be valid in a properties file (eg: true and false or numbers are converted to their real values).
+    *
+    * @param {String} key
+    * @param {String} value
+    * @return {PropertiesReader}
+    */
    PropertiesReader.prototype.set = function(key, value) {
-      this._addProperty(key + '=' + value);
+      var parsedValue;
+      if(!isNaN(value)) {
+         parsedValue = +value;
+      }
+      else if(value == 'true' || value == 'false') {
+         parsedValue = (value == 'true');
+      }
+      else {
+         parsedValue = ('' + value).trim();
+      }
+
+      this._properties[key] = parsedValue;
+
+      var expanded = key.split('.');
+      var source = this._propertiesExpanded;
+      while(expanded.length > 1) {
+         var step = expanded.shift();
+         source = (source[step] = source[step] || {});
+      }
+      source[expanded[0]] = parsedValue;
+
       return this;
    };
 
+   /**
+    * Gets the object that represents the exploded properties.
+    *
+    * Note that this object is currently mutable without the option to persist or interrogate changes.
+    *
+    * @return {*}
+    */
    PropertiesReader.prototype.path = function() {
       return this._propertiesExpanded;
-   };
-
-   PropertiesReader.prototype._addProperty = function(propertyString) {
-      if(!!propertyString.trim()) {
-         var property = propertyString.split('=', 2);
-         var key = property[0].trim();
-         var value = property[1];
-
-         if(!isNaN(value)) {
-            value = +value;
-         }
-         else if(value == 'true' || value == 'false') {
-            value = (value == 'true');
-         }
-         else {
-            value = ('' + value).trim();
-         }
-
-         this._properties[key] = value;
-
-         var expanded = key.split('.');
-         var source = this._propertiesExpanded;
-         while(expanded.length > 1) {
-            var step = expanded.shift();
-            source = (source[step] = source[step] || {});
-         }
-         source[expanded[0]] = value;
-      }
    };
 
    /**
---
+++ test/sectionTest.js
@@ -0,0 +1,44 @@
+
+var Assertions = require('unit-test').Assertions,
+    Sinon = require('unit-test').Sinon,
+    TestCase = require('unit-test').TestCase,
+    FileSystem = require('fs'),
+    propertiesReader = require('../src/PropertiesReader.js'),
+    properties;
+
+function tempFile(content) {
+   tempFile.nextName = (tempFile.nextName || 0) + 1;
+   (tempFile.files = tempFile.files || []).push(__dirname + '/temp_file_' + tempFile.nextName + '.properties');
+   FileSystem.writeFileSync(tempFile.files[tempFile.files.length - 1], content, 'utf-8');
+}
+
+function givenFilePropertiesReader(content) {
+   tempFile(content);
+   properties = propertiesReader(tempFile.files[tempFile.files.length - 1]);
+}
+
+module.exports = new TestCase("Repeat Calls", {
+
+   setUp: function() {
+   },
+
+   tearDown: function() {
+      while(tempFile.files && tempFile.files.length) {
+         var filePath = tempFile.files.pop();
+         try {
+            FileSystem.unlink(filePath);
+         }
+         catch(e) {}
+      }
+   },
+
+   'test Able to read from a file with sections': function() {
+      givenFilePropertiesReader('some.property = Value\n\n' +
+                                 '[section]\n another.property = Something\n\n' +
+                                 '[blah]\n another.property = Something Else');
+
+      Assertions.assertEquals( properties.get('some.property'), 'Value', 'Values are read into the properties object');
+      Assertions.assertEquals( properties.get('section.another.property'), 'Something', 'Values are read into the properties object');
+      Assertions.assertEquals( properties.get('blah.another.property'), 'Something Else', 'Values are read into the properties object');
+   }
+});
