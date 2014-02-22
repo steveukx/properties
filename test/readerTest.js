c76f184ab6e1d540a9e8bf01ba089bcd82b04305
Steve King <steve@mydev.co>
2014-02-22 18:42:34 +0000

    Add ability to bind the properties to an express/connect server (or any other object that supports the `set` API)

--- test/readerTest.js
+++ test/readerTest.js
@@ -8,18 +8,26 @@ var Assertions = require('unit-test').Assertions,
 
 function tempFile(content) {
    tempFile.nextName = (tempFile.nextName || 0) + 1;
-   (tempFile.files = tempFile.files || []).push(__dirname + '/temp_file_' + tempFile.nextName + '.properties');
+   tempFile.files.push(__dirname + '/temp_file_' + tempFile.nextName + '.properties');
    FileSystem.writeFileSync(tempFile.files[tempFile.files.length - 1], content, 'utf-8');
 }
+
+tempFile.pushDir = function (path) {
+   tempFile.dirs.push(path);
+   return path;
+};
 
 function givenFilePropertiesReader(content) {
    tempFile(content);
    properties = propertiesReader(tempFile.files[tempFile.files.length - 1]);
+   return properties;
 }
 
-module.exports = new TestCase("Repeat Calls", {
+module.exports = new TestCase("General reader functionality", {
 
    setUp: function() {
+      tempFile.files = tempFile.files || [];
+      tempFile.dirs = tempFile.dirs || [];
    },
 
    tearDown: function() {
@@ -27,6 +35,13 @@ module.exports = new TestCase("Repeat Calls", {
          var filePath = tempFile.files.pop();
          try {
             FileSystem.unlink(filePath);
+         }
+         catch(e) {}
+      }
+      while(tempFile.dirs && tempFile.dirs.length) {
+         var dirPath = tempFile.dirs.pop();
+         try {
+            FileSystem.rmdirSync(dirPath);
          }
          catch(e) {}
       }
@@ -48,13 +63,21 @@ module.exports = new TestCase("Repeat Calls", {
       givenFilePropertiesReader('\n\nsome.property=Value\n\nfoo.bar = A Value');
 
       Assertions.assertEquals(properties.length, 2, 'Blank lines are not stored as properties');
-   }
-   ,
+   },
 
    'test Properties can be read back via their dot notation names': function() {
       givenFilePropertiesReader('\n\nsome.property=Value\n\nfoo.bar = A Value');
 
       Assertions.assertEquals(properties.path().some.property, 'Value', 'Read back along dot notation paths some.property');
       Assertions.assertEquals(properties.path().foo.bar, 'A Value', 'Read back along dot notation paths foo.bar');
+   },
+
+   'test Sets properties into an app': function () {
+      var app = {set: Sinon.spy()};
+      var properties = givenFilePropertiesReader('\n\nsome.property=Value\n\nfoo.bar = A Value').bindToExpress(app);
+
+      Assertions.assert(app.set.withArgs('properties', properties).calledOnce, 'The complete properties object should be set as "properties"');
+      Assertions.assert(app.set.withArgs('some.property', 'Value').calledOnce, 'Sets all properties');
+      Assertions.assert(app.set.withArgs('foo.bar', 'A Value').calledOnce, 'Sets all properties');
    }
 });
