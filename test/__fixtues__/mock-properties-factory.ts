421f3341105caeb55bac964628b95507f4fdd5e2
Steve King <steve@mydev.co>
2026-01-10 11:27:24 +0000

    Remove v2 fixture

--- test/__fixtues__/mock-properties-factory.ts
+++ test/__fixtues__/mock-properties-factory.ts
@@ -1,8 +1,6 @@
 import { createPropertiesReader, PropertiesFactoryOptions } from '../../src/reader';
 import { TestContext } from './create-test-context';
 
-const legacy = require('old-properties-reader');
-
 export async function propertiesFromFile({file}: TestContext, content: string, options: Pick<PropertiesFactoryOptions, 'allowDuplicateSections' | 'saveSections'> = {}) {
    return propertiesReaderFixture('', {
       ...options,
@@ -12,8 +10,6 @@ export async function propertiesFromFile({file}: TestContext, content: string, o
 
 export function propertiesReaderFixture(content: string | Buffer = '', options: Pick<PropertiesFactoryOptions, 'allowDuplicateSections' | 'saveSections' | 'sourceFile'> = {}) {
    const props = createPropertiesReader(options);
-
-   // const props = legacy( options.sourceFile, 'utf8', options);
    if (content) {
       props.read(content);
    }
